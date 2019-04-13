81a70a7f68172bf9003bb3a9c6f665cea87787e8
ripsildeveloper <ripsildeveloper3@gmail.com>
2019-04-13 23:23:29 +0530

    changes in order

--- src/app/order-management/view-order-details/view-order-details.component.ts
+++ src/app/order-management/view-order-details/view-order-details.component.ts
@@ -3,6 +3,7 @@ import {Route, ActivatedRoute, Router} from '@angular/router';
 import {FormControl, FormGroup, FormBuilder} from '@angular/forms';
 import {OrderManagementService} from '../order-management.service';
 import {Order} from '../view-orders/order.model';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-view-order-details',
@@ -13,8 +14,12 @@ export class ViewOrderDetailsComponent implements OnInit {
   id;
 orderModel: Order;
 orderForm: FormGroup;
-OrderStatus = ['Processing', 'OnHold', 'Completed' , 'Cancelled', 'Failed'];
+productModel: any;
+message;
+action;
+OrderStatus = ['New', 'Processing', 'OnHold', 'Completed' , 'Cancelled', 'Failed'];
   constructor(private router: Router,  private orderService: OrderManagementService, private route: ActivatedRoute,
+    private snackBar: MatSnackBar,
     private fb: FormBuilder) {
     this.id = this.route.snapshot.params.id;
   }
@@ -22,6 +27,7 @@ OrderStatus = ['Processing', 'OnHold', 'Completed' , 'Cancelled', 'Failed'];
   ngOnInit() {
     this.createForm();
     this.viewOrderDetails();
+    this.getProducts();
   }
   createForm() {
 this.orderForm = this.fb.group({
@@ -34,6 +40,27 @@ viewOrderDetails() {
     this.orderModel = data[0];
   }, err => {
 console.log(err);
+  });
+}
+getProducts() {
+  this.orderService.getProducts().subscribe(data => {
+    this.productModel = data;
+  }, err => {
+    console.log(err);
+  });
+}
+updateStatus()  {
+  this.message = 'Order Updated';
+  this.orderModel = new Order();
+  this.orderModel.orderStatus = this.orderForm.controls.statusType.value;
+  this.orderService.updateStatus(this.id, this.orderModel).subscribe(data => {
+    this.orderModel = data[0];
+    this.snackBar.open(this.message, this.action, {
+      duration: 3000,
+    });
+    this.router.navigate(['orders/vieworders']);
+  }, err => {
+    console.log(err);
   });
 }
 }
